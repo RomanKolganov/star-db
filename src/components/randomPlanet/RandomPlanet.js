9ec2e2591072ca3556ed3cab0fa462a00868f69f
roman <ramoni1990@gmail.com>
2020-03-29 18:23:03 +0300

    errors in random planet

--- src/components/randomPlanet/RandomPlanet.js
+++ src/components/randomPlanet/RandomPlanet.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import SwapiService from "../../service/SwapiService";
+import Spinner from "../spinner/Spinner";
+import ErrorIndicator from "../errorIndicator/ErrorIndicator";
 import "./randomPlanet.css";
 
 export default class RandomPlanet extends Component {
@@ -7,57 +9,79 @@ export default class RandomPlanet extends Component {
     super();
 
     this.state = {
-      id: null,
-      population: null,
-      rotationPeriod: null,
-      diameter: null,
-      name: null
+      planet: {},
+      loading: true,
+      error: false
     };
 
     this.updatePlanet();
   }
   swapiService = new SwapiService();
 
-  updatePlanet() {
-    const id = Math.floor(Math.random() * 25) + 2;
-    this.swapiService.getPlanet(id).then(planet => {
-      this.setState({
-        id: id,
-        population: planet.population,
-        rotationPeriod: planet.rotation_period,
-        diameter: planet.diameter,
-        name: planet.name
-      });
+  onPlanetLoaded = planet => {
+    this.setState({ planet, loading: false });
+  };
+
+  onError = err => {
+    this.setState({
+      error: true,
+      loading: false
     });
+  };
+
+  updatePlanet() {
+    // const id = 100000000;
+    const id = Math.floor(Math.random() * 25) + 2;
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
-    const { id, population, rotationPeriod, diameter, name } = this.state;
+    const { planet, loading, error } = this.state;
+
+    const hasData = !(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = hasData ? <PlanetView planet={planet} /> : null;
     return (
       <div className="random-planet jumbotron rounded">
-        <img
-          className="planet-image"
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-          alt="planet"
-        />
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
+        {errorMessage}
+        {spinner}
+        {content}
       </div>
     );
   }
 }
+
+const PlanetView = ({ planet }) => {
+  const { id, population, rotationPeriod, diameter, name } = planet;
+  return (
+    <>
+      <img
+        className="planet-image"
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+        alt="planet"
+      />
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
--- src/service/SwapiService.js
+++ src/service/SwapiService.js
@@ -1,6 +1,46 @@
 export default class SwapiService {
   _apiBase = "https://swapi.co/api";
 
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  _transformPlanet(planet) {
+    const id = this._extractId(planet);
+    return {
+      id: id,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter,
+      name: planet.name
+    };
+  }
+
+  _transformStarship(starship) {
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargoCapacity
+    };
+  }
+
+  _transformPerson(person) {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor
+    };
+  }
+
   async getRecource(url) {
     const res = await fetch(`${this._apiBase}${url}`);
 
@@ -13,28 +53,31 @@ export default class SwapiService {
 
   async getAllPeople() {
     const res = await this.getRecource(`/people`);
-    return res.results;
+    return res.results.map(this._transformPerson);
   }
 
-  getPerson(id) {
-    return this.getRecource(`/people/${id}`);
+  async getPerson(id) {
+    const res = await this.getRecource(`/people/${id}`);
+    return this._transformPerson(res);
   }
 
   async getAllPlanets() {
     const res = await this.getRecource(`/planets`);
-    return res.results;
+    return res.results.map(this._transformPlanet);
   }
 
-  getPlanet(id) {
-    return this.getRecource(`/planets/${id}`);
+  async getPlanet(id) {
+    const planet = await this.getRecource(`/planets/${id}`);
+    return this._transformPlanet(planet);
   }
 
   async getAllStarships() {
     const res = await this.getRecource(`/starships`);
-    return res.results;
+    return res.results.map(this._transformStarship);
   }
 
-  getStarship(id) {
-    return this.getRecource(`/starships/${id}`);
+  async getStarship(id) {
+    const res = await this.getRecource(`/starships/${id}`);
+    return this._transformStarship(res);
   }
 }
