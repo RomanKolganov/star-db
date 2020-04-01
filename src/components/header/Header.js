bad9a249c3137f83cf1eb9536cd0c552b2c76b19
roman <ramoni1990@gmail.com>
2020-04-01 17:26:34 +0300

    relative routes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,9 +9,9 @@ const Header = ({onServiceChange}) => {
         <h3><Link to="/">Star DB</Link></h3>
 
         <ul className="d-flex">
-            <li><Link to="/people">People</Link></li>
-            <li><Link to="/planets">Planets</Link></li>
-            <li><Link to="/starships">Starships</Link></li>
+            <li><Link to="/people/">People</Link></li>
+            <li><Link to="/planets/">Planets</Link></li>
+            <li><Link to="/starships/">Starships</Link></li>
         </ul>
 
         <button
